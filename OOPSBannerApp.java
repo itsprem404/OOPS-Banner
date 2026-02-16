e6cfc53313469cc2593f6ae79d366e5d1605038b
Prem <ppremkkumar010@gmail.com>
2026-02-16 14:12:09 +0530

    UC1: Print OOPS banner using print statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * UC: Print "OOPS" as a banner using * and spaces
+ *
+ * @author Prem
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****   *****");
+        System.out.println("*     *  *     *  *     *  *");
+        System.out.println("*     *  *     *  *     *  *");
+        System.out.println("*     *  *     *  * ****    ****");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println(" *****    *****   *         *****");
+
+    }
+}
